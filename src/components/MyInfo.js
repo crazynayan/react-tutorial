8b5bfcabd468fa6fcc8501bc8ce5e0bf675af9cb
crazynayan <nayan@crazyideas.co.in>
2020-07-23 12:17:17 +0530

    first commit

---
+++ src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Nayan Zaveri</h1>
+      <p>I am learning React</p>
+      <ol>
+        <li>Swiss</li>
+        <li>Orlando</li>
+        <li>California</li>
+      </ol>
+    </div>
+  )
+}
+
+export default MyInfo
